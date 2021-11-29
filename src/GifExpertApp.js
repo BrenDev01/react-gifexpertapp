99203998c91b038931e493273fd4aca2901bdb2c
bpadilla <bpadilla.coop@gmail.com>
2021-11-29 11:43:07 -0600

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+export const GifExpertApp = () => {
+
+const [categories, setCategories] = useState(['Naturo']);
+
+// const handleAdd = () => {
+//     //setCategories([...categories, 'One Piece']);
+//     setCategories(cats => [...cats, 'HunterXHunter']);
+// }
+
+    return (
+        <>
+            <h2>GiftExpertApp</h2>
+            <AddCategory setCategories = {setCategories}/>
+            <hr />
+
+            <ol>
+                {
+                    categories.map( category => (
+                        <GifGrid 
+                        key = {category}
+                        category = {category}
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    )
+}
